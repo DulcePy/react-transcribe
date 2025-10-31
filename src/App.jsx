af537e61a42809d5771a1437e07a5de9467467c0
Dulce Figueredo <dulcefigueredo2252@gmail.com>
2025-10-31 15:26:38 -0300

    Started a new project with React + Vite + Tailwind CSS.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-blue-600 text-5xl font-bold">React TranScribe</h1>
+    </div>
+  );
+}
+
+export default App;
