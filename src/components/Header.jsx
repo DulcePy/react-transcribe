f959387803718bf148d779690a241e234374b705
Dulce Figueredo <dulcefigueredo2252@gmail.com>
2025-11-03 16:38:00 -0300

    Implement audio file handling and display components; enhance UI with new styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
         Free<span className="text-blue-600 bold">Scribe</span>
       </h1>
 
-      <button className="flex items-center gap-2">
+      <button className="flex items-center gap-2 px-3 py-2 rounded-lg text-blue-400 cursor-pointer specialBtn">
         <p>New</p>
         <i className="fa-solid fa-plus"></i>
       </button>
